006fe4c6d4b703eadaff601d10ae2dca23d32e56
Galina Berezovskaya <berezovskayago@mail.ru>
2025-03-25 22:35:06 +0500

    валидация

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
- 
-
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  await app.listen(3000);
+  // Глобальная валидация (добавлено)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,            // Автоматическое преобразование типов
+      disableErrorMessages: false, // Показывать сообщения об ошибках
+      whitelist: true,            // Удаляет лишние поля
+      forbidNonWhitelisted: true   // Возвращает ошибку при лишних полях
+    })
+  );
 
+  await app.listen(3000);
 }
 
 bootstrap();
